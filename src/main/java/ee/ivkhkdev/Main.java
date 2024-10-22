76ffd009309e494ac6f268ba4c3685e53d967263
Ares/Hobie <reijisaan@gmail.com>
2024-10-22 12:44:21 +0300

    first commit

---
+++ src/main/java/ee/ivkhkdev/Main.java
@@ -0,0 +1,22 @@
+package ee.ivkhkdev;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 3
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("enter name: ");
+        String name = sc2.nextLine();
+        System.out.print("enter last name: ");
+        String lname = sc2.nextLine();
+        System.out.print("enter birth month (ex 'January'): ");
+        String bmonth = sc2.nextLine();
+        System.out.print("enter birth day (ex 4): " );
+        int bday = sc2.nextInt();
+        System.out.print("enter birth year (ex '1984'): ");
+        int byear = sc2.nextInt();
+        System.out.println(name + " " + lname + "born on" + bday + " " + bmonth + " " + byear + " ");
+        
+    }
+}
